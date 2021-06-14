81a83373876ddf105f03164f42b66daf515459e5
zishan100 <Zishanansari111@gmail.com>
2021-06-14 21:12:48 +0530

    Updates

---
+++ precache-manifest.364268ec666efdc02d704c9670f62b61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ebd2a9613ed969c1b1d",
+    "url": "/ShopApp-Using-ReactJS/static/css/main.fbae122f.chunk.css"
+  },
+  {
+    "revision": "9ebd2a9613ed969c1b1d",
+    "url": "/ShopApp-Using-ReactJS/static/js/main.0893ec10.chunk.js"
+  },
+  {
+    "revision": "6b6d963a2b2f7eb81c0e",
+    "url": "/ShopApp-Using-ReactJS/static/js/runtime~main.2eb52199.js"
+  },
+  {
+    "revision": "dc799cc54c83b998ea43",
+    "url": "/ShopApp-Using-ReactJS/static/js/2.e6a16d3e.chunk.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ShopApp-Using-ReactJS/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ShopApp-Using-ReactJS/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "e383bd3fbad219173791757455716a56",
+    "url": "/ShopApp-Using-ReactJS/index.html"
+  }
+];
